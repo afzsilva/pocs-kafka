701bc9fd522ec824e922cb67e014621a46edd71b
afzsilva <afzsilva@gmail.com>
2024-06-30 11:01:29 -0300

    Atualiza topico objeto

--- demo-app-consumer/src/main/java/com/example/demoappconsumer/listner/ConsumerListner.java
+++ demo-app-consumer/src/main/java/com/example/demoappconsumer/listner/ConsumerListner.java
@@ -12,18 +12,19 @@ import java.time.LocalDateTime;
 public class ConsumerListner {
 
     final String TOPICO_TESTE = "meu_topico_teste";
+    final String TOPICO_OBJETO = "meu_topico_objeto";
 
     @KafkaListener(topics = TOPICO_TESTE , groupId = "grupo-1")
     public void listen(String message){
         System.out.println("RECEBIDO ---> "+message+" : "+ LocalDateTime.now());
     }
 
-    @KafkaListener(topics ="meu_topico_objeto" , groupId = "grupo-1")
+    @KafkaListener(topics =TOPICO_OBJETO , groupId = "grupo-1")
     public void listen(Pessoa pessoa){
         System.out.println("RECEBIDO ---> "+pessoa+" : "+ LocalDateTime.now());
     }
 
-    @KafkaListener(topics ="meu_topico_objeto" , groupId = "grupo-1")
+    @KafkaListener(topics =TOPICO_OBJETO , groupId = "grupo-1")
     public void listenDinamico(Cidade cidade){
         System.out.println("RECEBIDO ---> "+cidade+" : "+ LocalDateTime.now());
     }
