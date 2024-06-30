6f03151602386a8410581ae6a36158b385bdf6a9
afzsilva <afzsilva@gmail.com>
2024-06-30 10:57:15 -0300

    Modifica topico para constatnte

--- demo-app-consumer/src/main/java/com/example/demoappconsumer/listner/ConsumerListner.java
+++ demo-app-consumer/src/main/java/com/example/demoappconsumer/listner/ConsumerListner.java
@@ -11,7 +11,9 @@ import java.time.LocalDateTime;
 @Component
 public class ConsumerListner {
 
-    @KafkaListener(topics ="meu_topico_teste" , groupId = "grupo-1")
+    final String TOPICO_TESTE = "meu_topico_teste";
+
+    @KafkaListener(topics = TOPICO_TESTE , groupId = "grupo-1")
     public void listen(String message){
         System.out.println("RECEBIDO ---> "+message+" : "+ LocalDateTime.now());
     }
