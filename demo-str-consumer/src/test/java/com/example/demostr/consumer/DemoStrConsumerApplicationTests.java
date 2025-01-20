e30ba81f71006762087ff6e686f6b3888eba0605
afzsilva <afzsilva@gmail.com>
2025-01-20 16:36:29 -0300

    Modificações consumer

---
+++ demo-str-consumer/src/test/java/com/example/demostr/consumer/DemoStrConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demostr.consumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoStrConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
